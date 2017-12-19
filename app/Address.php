080591d6991b88bda75e23f385a638a7ba5c0298
Luiz Gabriel <luizgabriel.info@gmail.com>
2017-12-18 22:31:13 -0300

    Added map view

--- app/Address.php
+++ app/Address.php
@@ -4,19 +4,13 @@ namespace GeoLV;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * Class Address
  * @package GeoLV
  * @method static Search|Model firstOrCreate(array $data)
  * @property int $relevance
- * @property double latitude
- * @property double longitude
- * @property double rad_latitude
- * @property double rad_longitude
- * @property double x
- * @property double y
- * @property double z
  */
 class Address extends Model
 {
@@ -48,41 +42,9 @@ class Address extends Model
         return preg_replace('/\D/', '', $value);
     }
 
-    public function getRadLatitudeAttribute()
-    {
-        return deg2rad($this->latitude);
-    }
-
-    public function getRadLongitudeAttribute()
-    {
-        return deg2rad($this->longitude);
-    }
-
-    public function getXAttribute()
-    {
-        return cos($this->rad_latitude) * cos($this->rad_longitude);
-    }
-
-    public function getYAttribute()
-    {
-        return cos($this->rad_latitude) * sin($this->rad_longitude);
-    }
-
-    public function getZAttribute()
-    {
-        return sin($this->rad_latitude);
-    }
-
     public function getHashCode()
     {
         $data = array_except($this->attributesToArray(), ['id', 'relevance', 'text', 'created_at', 'updated_at', 'search_id']);
         return md5(json_encode($data));
     }
-
-    public function newCollection(array $models = [])
-    {
-        return new AddressCollection($models);
-    }
-
-
 }
