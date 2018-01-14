e566c797ccc8076e2446d5154c3d76e2a868b4ce
Luiz Gabriel <luizgabriel.info@gmail.com>
2018-01-14 17:05:24 -0300

    Fixed api request

--- app/Http/Controllers/Api/GeocodingController.php
+++ app/Http/Controllers/Api/GeocodingController.php
@@ -22,11 +22,16 @@ class GeocodingController extends Controller
 
     public function geocode(GeocodingRequest $request)
     {
-        $address = join(" ", $request->only(['street_name', 'locality', 'cep']));
-        $match = (new Dictionary())->getMatchingQuery($address);
-        $results = $this->geocoder->geocode($match)->get()->map(function (Address $address) {
-            return array_except($address->toArray(), ['id', 'created_at', 'updated_at', 'text']);
-        });
+        $text = ucwords((new Dictionary())->getMatchingQuery($request->get('text')));
+        $locality = $request->get('locality');
+        $postalCode = $request->get('postal_code');
+
+        $results = $this->geocoder
+            ->geocode($text, $locality, $postalCode)
+            ->get()
+            ->map(function (Address $address) {
+                return array_except($address->toArray(), ['id', 'created_at', 'updated_at', 'text']);
+            });
 
         return $this->api($results);
     }
