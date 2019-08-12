f22967fdf16413c4c9c0c73c4898789ac397fe5c
Luiz Gabriel <luizgabriel.info@gmail.com>
2019-08-12 12:59:41 -0300

    added aws support

--- app/Jobs/GeocodeNextFile.php
+++ app/Jobs/GeocodeNextFile.php
@@ -19,7 +19,7 @@ class GeocodeNextFile implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    const CHUNK_SIZE = 200;
+    const CHUNK_SIZE = 100;
 
     public function handle(GeocodingFileProcessor $processor)
     {
