b4aa69d343a744e6037d10ed2f4e499e2f1518b9
Luiz Gabriel <luizgabriel.info@gmail.com>
2019-08-13 13:06:58 -0300

    Updated telescope prune schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('telescope:prune')->dailyAt('02:00');
+        $schedule->command('telescope:prune --hours=48')->daily();
     }
 
     /**
